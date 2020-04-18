db46c11246f6727d73cb40da7b0cf1407c02a23b
Richard Hanks <richardhanksjr@gmail.com>
2020-04-17 22:58:47 -0400

    Added localhost to settings.  Added viewport meta tag in base html.

--- budget_app/settings.py
+++ budget_app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config['SETTINGS']['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['161.35.96.45']
+ALLOWED_HOSTS = ['161.35.96.45', 'localhost']
 
 
 # Application definition
