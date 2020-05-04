af5f7d85918ee686f3e649a2ad4d326d85d27d05
Richard Hanks <richardhanksjr@gmail.com>
2020-05-04 11:47:53 -0400

    Removed nohup from the deploy script.

--- deploy_to_run_server.sh
+++ deploy_to_run_server.sh
@@ -8,5 +8,5 @@ ps aux | grep manage.py
 REVISION=$(ps aux | grep manage.py | awk  '{print $2}')
 kill $REVISION | cut -d" " -f1
 echo "Restarting runserver in background on port 5000."
-nohup pipenv run python manage.py runserver 0.0.0.0:5000 > nohup.out &
+pipenv run python manage.py runserver 0.0.0.0:5000 > nohup.out &
 return 0
