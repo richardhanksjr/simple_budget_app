44ef397fdef65a67c07dab6dd9517702fdda945b
Richard Hanks <richardhanksjr@gmail.com>
2020-06-24 15:35:46 -0400

    Changed STATIC_ROOT back to static

--- budget_app/settings.py
+++ budget_app/settings.py
@@ -143,7 +143,7 @@ STATICFILES_FINDERS = [
 ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
